0f71dc03c9c6957b924d6d68ca0ad53e73d4fce2
henimia <henimia@gmail.com>
2021-04-29 11:12:48 +0200

    Add képesítő Algo

--- src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
+++ src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
@@ -1,5 +1,28 @@
 package hu.nive.ujratervezes.adddigits;
 
 public class AddDigits {
-    //todo: implement your solution here
+
+    public int addDigits(String input) {
+        if (input == null || input.equals("")) {
+            return -1;
+        }
+        int sum = 0;
+        for (int i = 0; i < input.length(); i++) {
+            try {
+                sum += Integer.parseInt(String.valueOf(input.charAt(i)));
+            } catch (NumberFormatException e) {
+                continue;
+            }
+
+        }
+        return sum;
+    }
+
 }
+
+
+
+
+
+
+
